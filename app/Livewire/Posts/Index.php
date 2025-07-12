13cf96ceb4a267ee93dc020dccb24c44bd6f270d
Julian Grisales <jgrisales@gmail.com>
2025-07-12 10:39:16 +0200

    Products table

--- app/Livewire/Posts/Index.php
+++ app/Livewire/Posts/Index.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire\Posts;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $products;
+
+    public function mount()
+    {
+        $this->products = Product::take(10)->get();
+    }
+
     public function render()
     {
         return view('livewire.posts.index');
--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -1,3 +1,30 @@
 <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-    Post List here
+    <div class="overflow-hidden w-full overflow-x-auto rounded-radius border border-outline dark:border-outline-dark">
+        <table class="w-full text-left text-sm text-on-surface dark:text-on-surface-dark">
+            <thead class="border-b border-outline bg-surface-alt text-sm text-on-surface-strong dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark-strong">
+                <tr>
+                    <th scope="col" class="p-4">ProductID</th>
+                    <th scope="col" class="p-4">Name</th>
+                    <th scope="col" class="p-4">Stock</th>
+                    <th scope="col" class="p-4">Price</th>
+                    <th scope="col" class="p-4">Created At</th>
+                </tr>
+            </thead>
+            <tbody class="divide-y divide-outline dark:divide-outline-dark">
+                @forelse ($products as $product)
+                    <tr>
+                        <td class="p-4">{{ $product->id }}</td>
+                        <td class="p-4">{{ $product->name }}</td>
+                        <td class="p-4">{{ $product->stock }}</td>
+                        <td class="p-4">{{ $product->price }}</td>
+                        <td class="p-4">{{ $product->created_at }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="5" class="p-4 text-center">No products found.</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 </div>
